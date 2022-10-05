f4b6737a3ef465b6e0a78e8a751a3747b9e398b5
DESKTOP-D7KKILL\aip65 <aip65692@gmail.com>
2022-10-05 19:54:31 +0300

    * Сделал страницу "Оставить тикет" * Подправил кнопки (стали зеленые + снизу немного черные)

--- js/script.js
+++ js/script.js
@@ -1,113 +1,129 @@
-// [titcked] Отправка формы на почту
+// // [titcked] Отправка формы на почту
 
-document.addEventListener('DOMContentLoaded', function (){
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend); // При отправки формы мы должны перейти - formSend
+// document.addEventListener('DOMContentLoaded', function (){
+//     const form = document.getElementById('form');
+//     form.addEventListener('submit', formSend); // При отправки формы мы должны перейти - formSend
+
+//     // Запрещяем стандартную отправку формы
+//     async function formSend(e) {
+//         e.preventDefault();
+
+//         let error = formValidate(form);
+
+//         // Отправление формы
+//         let formDate = new FormData(form);
+//         formDate.append('image', formImage.files[0]);
+
+//         if (error === 0){
+//             form.classList.add('_sending');
+//             let response = await fetch('sendmail.php', {
+//                 mettod: 'POST',
+//                 body: formDate
+//             });
+//             if (response.ok) {
+//                 let result = await response.json();
+//                 alert(result.message);
+//                 formPreview.innerHTML = '';
+//                 form.reset();
+//                 form.classList.remove('_sending');
+//             }else {
+//                 alert("Ошибка")
+//                 form.classList.remove('_sending');
+//             }
+//         }else
+//             alert('Заполните поле');
+//     }
+
+//         function formValidate(form){
+//             let error = 0;
+//             let formReq = document.querySelectorAll('._req') // Делаем обязательное поле
+
+//             for (let index = 0; index < formReq.length; index++) {
+//                 const input = formReq[index];
+//                 formRemoveError(input);
+
+//                 if (input.classList.contains('_email')){
+//                     if(emailTest(input)) {
+//                         formAddError(input);
+//                         error++
+//                     }
+//                     // Проверка чек бокса
+//                 }else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+//                     formAddError(input);
+//                     error++;
+//                 }else
+//                     // Проверка заполнено ли проле вообще
+//                     if (input.value === '') {
+//                         formAddError(input);
+//                         error++;
+//                     }
+//             }
+//             return error;
+//         }
+
+
+//         //Подчеркивает красным
+//         function formAddError(input) {
+//             input.parentElement.classList.add('_error');
+//             input.classList.add('_error');
+//         }
+//         // Есть все поля заполненны коректно то убирает обводку
+//         function formRemoveError(input) {
+//             input.parentElement.classList.remove('_error');
+//             input.classList.remove('_error');
+//         }
+//          //Функия теста email [Проверяет есть ли собачка или точка]
+//          function emailTest(input) {
+//              return !/^\w+([\.-]?\ц+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//          }
     
+//         // Получаем инпут file в переменную
+//         const formImage = document.getElementById('formImage');
+//         // Получает Div для превью в переменную
+//         const formPreview = document.getElementById('formPreview');
 
-    // Запрещяем стандартную отправку формы
-    async function formSend(e) {
-        e.preventDefault();
+//         formImage.addEventListener('change', () => {
+//            uploadFile(formImage.files[0]); 
+//         });
 
-        let error = formValidate(form);
+//         function uploadFile(file) {
+//             // Проверяем тип файла
+//             if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+//                 alert('Разрешены только изображения');
+//                 formImage.value = '';
+//                 return;
+//             }
+//             // Проверяем размер файла
+//             if(file.size > 2 * 1024 * 1024){
+//                 alert('Файл должен быть менее 2 мб');
+//                 return;
+//             }
 
-        if (error === 0){
+//             // Показ картинки на странице
+//             var reader = new FileReader();
+//             reader.onload = function (e) {
+//                 formPreview.innerHTML = `<img src= "${e.target.result}" alt="Фото">`;
+//             };
+//             reader.onerror = function (e) {
+//                 alert('Ошибка');
+//             };
+//             reader.readAsDataURL(file);
             
-        }else
-            alert('Заполните поле');
-    }
+//         }
+//         document.querySelectorAll('.accordion').forEach((el) => {
+//             el.addEventListener('click', () => {
+                
+//                 let content = el.nextElementsSibling;
+//                 console.log(content)
+//                 if(content.style.maxHeight){
+//                     document.querySelectorAll('.content').forEach((el) => el.style.maxHeight = null)
+//                 }else{
+//                     document.querySelectorAll('.content').forEach((el) => el.style.maxHeight = null)
+//                     content.style.maxHeight = content.srollHHeight + 'px'
+//                 }
+//             })
+//         })
+// });
 
-        function formValidate(form){
-            let error = 0;
-            let formReq = document.querySelectorAll('._req') // Делаем обязательное поле
 
-            for (let index = 0; index < formReq.length; index++) {
-                const input = formReq[index];
-                formRemoveError(input);
-
-                if (input.classList.contains('_email')){
-                    if(emailTest(input)) {
-                        formAddError(input);
-                        error++
-                    }
-                    // Проверка чек бокса
-                }else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-                    formAddError(input);
-                    error++;
-                }else
-                    // Проверка заполнено ли проле вообще
-                    if (input.value === '') {
-                        formAddError(input);
-                        error++;
-                    }
-            }
-        }
-
-        // return error;
-
-        //Подчеркивает красным
-        function formAddError(input) {
-            input.parentElement.classList.add('_error');
-            input.classList.add('_error');
-        }
-        // Есть все поля заполненны коректно то убирает обводку
-        function formRemoveError(input) {
-            input.parentElement.classList.remove('_error');
-            input.classList.remove('_error');
-        }
-         //Функия теста email [Проверяет есть ли собачка или точка]
-         function emailTest(input) {
-             return !/^\w+([\.-]?\ц+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-         }
-    
-        // Получаем инпут file в переменную
-        const formImage = document.getElementById('formImage');
-        // Получает Div для превью в переменную
-        const formPreview = document.getElementById('formImage');
-
-        formImage.addEventListener('change', () => {
-           uploadFile(formImage.files[0]); 
-        });
-
-        function uploadFile(file) {
-            // Проверяем тип файла
-            if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-                alert('Разрешены только изображения');
-                formImage.value = '';
-                return;
-            }
-            // Проверяем размер файла
-            if(file.size > 2 * 1024 * 1024){
-                alert('Файл должен быть менее 2 мб');
-            }
-
-            // Показ картинки на странице
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                formPreview.innerHTML = '<img src="${e.target.result}" alt="Фото">';
-            };
-            reader.onerror = function (e) {
-                alert('Ошибка');
-            };
-            reader.readAsDataURL(file);
-            
-        }
-        
-        // Аккардеон[spoiler]//////////////////////////////////////////////////////////////////////////////////////////
-        const spoilersArray = document.querySelectorAll('[data-spoIler]');
-        if (spoilersArray.length > 0) {
-            //Получение обычнх слойлеров
-            const spoilerRegular = Array.from(spoilersArray).filter(function (item, index, self) {
-                return !item.dataset.spoIler.split(",")[0];
-            });
-            // Иницилизация обычных спойлеров
-            if(spoilerRegular.length > 0){
-                initSpoIler(spoilerRegular);
-            }
-
-            const spoIlerMedia = Array.from(spoilersArray).filter(function (item, index, self) {
-                return item.dataset.spoIler.
-            }
-        }
-});
 
--- sendmail.php
+++ sendmail.php
@@ -1,71 +1,78 @@
 <?php
-    use PHPMailer\PHPMailer;
-    use PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
 
-    $mail = new PHPMailer (true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLaunguage('ru', 'phpmailer/language/');
-    $mail->IsHTML(true);
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
 
-    // От кого письмо
-    $mail->setFrom('info@mail.ru', 'SellYES - Поддержка');
-    // Кому отправить
-    $mail->addAddres('aip6569@indox.ru');
-    // Тема письма
-    $mail->Subject = 'Привет это Айрат';
+// От кого письмо
+$mail->setFrom('alkamo@inbox.ru', 'Вижу цель');
+// Кому отправить
+$mail->addAddress('sellyeshelp@gmail.com');
+// Тема письма
+$mail->Subject = "Заявка с сайта";
 
-    // Рука
-    $hand = 'Правая';
-    if($_POST['hand'] == "left"){
-        $hand = "Левая";
+$mail->isSMTP(); //Send using SMTP
+$mail->Host = 'smtp.example.com';  //Set the SMTP server to send through
+$mail->SMTPAuth = true;  //Enable SMTP authentication
+$mail->Username  = 'user@example.com'; //SMTP username
+$mail->Password  = 'secret';  //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  //Enable implicit TLS encryption
+$mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+// Рука :)
+$hand = "Правая";
+if($_POST['hand'] == 'left'){
+    $hand = "Левая";
+}
+
+// Тело письма
+$body = '<h1>Встречай письмо!</h1>';
+
+if(trim(!empty($_POST['name']))){
+    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+}
+if(trim(!empty($_POST['hand']))){
+    $body.='<p><strong>Рука:</strong> '.$hand.'</p>';
+}
+if(trim(!empty($_POST['aga']))){
+    $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+}
+if(trim(!empty($_POST['message']))){
+    $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+}
+
+// Прикрепить файл
+if (!empty($_FILES['image']['tmp_name'])) {
+    // путь загрузки файла
+    $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+    // грузим файл
+    if(copy($_FILES['image']['tmp_name'], $filePath)){
+        $fileAttach = $filePath;
+        $body.='<p><strong>Фото в приложении</strong>';
+        $mail->addAttachment($fileAttach);
     }
+}
 
-    // Тело письма
-    $body = '<h1>Встречайте супер письмо<h1>';
+$mail->Body = $body;
 
-    if(trim(!empty($_POST['name']))){
-        $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-    }
-    if(trim(!empty($_POST['email']))){
-        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
-    }
-    if(trim(!empty($_POST['hand']))){
-        $body.='<p><strong>Рука:</strong> '.$hand.'</p>';
-    }
-    if(trim(!empty($_POST['age']))){
-        $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
-    }
-    if(trim(!empty($_POST['message']))){
-        $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
-    }
+// Отправляем
+if (!$mail->send()) {
+    $message = 'Ошибка';
+} else {
+    $message = 'Данные отправлены!';
+}
 
-    // Прикрепряем файлы
-    if(!empty($_FILES['image']['tmp_name'])) {
-        //Путь загрузки файла
-        $filePath = __DIR__ . "/file/" . $_FILES['image']['name'];
-        //Грузим файлы
-        if(copy($_FILES['image']['tmp_name'], $filePath)){
-            $fileAttach = $filePath;
-            $body.='<p><strong>Фото в приложении</strong>';
-            $mail->addAttachment($fileAttach);
-        }
-    }
+$response = ['message' => $message];
 
-    $mail->Body = $body
-
-    if (!$mail->send()) {
-        $message = 'Ошибка';
-    }else
-        $message = 'Данные отправлены';
-
-    $response = ['message' => $message];
-
-    header('Content-type: application/json');
-    echo json_encode($response);
+header('Content-type: application/json');
+echo json_encode($response);
 ?>
-
-
-
