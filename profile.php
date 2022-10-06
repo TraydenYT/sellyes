9d43fe61bffcaed8a9b2602a9cc8313f105cb0ce
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2022-10-06 20:57:56 +0300

    Заготовка для меню профиля

--- profile.php
+++ profile.php
@@ -5,16 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SELLYES</title>
-    <link rel="shortcut icon" href="img/icons/favicon.ico" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="css/bootstrap-reboot.min.css"> 
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/font.css">
     <link rel="stylesheet" href="css/slick.css">
-    <script src="https://kit.fontawesome.com/9314941886.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    
+
 </head>
 <body>
-      <section class="promo">
+<section class="promo">
         <div class="container-fluid">
             <header class="header d-flex">
                     <div class="header_navigation">
@@ -50,51 +52,24 @@
                             </div>
                             <button class="button lang" key="Authorization" onclick="window.location.href='./registration.php'">Авторизация</button>
                         </ul>
-                    </div>        
+                    </div>
+                   
             </header>
-
-
-   <footer class="footer">
-        <div class="container-fluid">
-            <nav class="nav d-flex">
-                <ul class="menu d-flex">
-                    
-                                <li class="menu__list">
-                                    <a href="#mobile" class="menu__link lang footer__list" key="Sell">Условия использования</a>
-                                </li>
-                                <li class="menu__list">
-                                    <a href="#" class="menu__link lang footer__list" key="Reviews">Политика приватности</a>
-                                </li>
-                                <li class="menu__list">
-                                    <a href="#" class="menu__link lang footer__list" key="Guarantees">Помощь</a>
-                                </li>
-                                <li class="menu__list">
-                                    <a href="#" class="menu__link lang footer__list" key="freebie">Вакансии</a>
-                                </li>
-                                <li class="menu__list">
-                                    <a href="ticket.php" class="menu__link lang footer__list" key="help">Оставить тикет</a>
-                                </li>
-                </ul>
-
-                <div class="footer__social">
-                    <a href="#" target="_blank" class="footer__item">
-                        <i class="fa-brands fa-steam"></i>
-                    </a>
-                    <a href="#" class="footer__item">
-                        <i class="fa-brands fa-youtube"></i>
-                    </a>
-
-                    <a href="#" class="footer__item">
-                        <i class="fa-brands fa-vk"></i>
-                    </a>
-
-                    <a href="#" class="footer__item">
-                        <i class="fa-brands fa-telegram"></i>
-                    </a>
-                </div>
-            </nav>
-        </div>
-   </footer>
+   
+            <section class="page-section">
+                    <div class="wrap container">
+                        <div class="row profile">
+                            <div class="col-lg-3 col-md-3">
+                                <input type="hidden" id="state" value="normal" />
+                                <div class="widget account-details">
+                                    <div class="information-title">
+                                        Мой профиль
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
